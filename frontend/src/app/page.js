91b0b3525f6e338693d1e33194e728a87d902d6a
Aliaksei Lomats <alexeyttdeveloper@gmail.com>
2023-06-06 14:22:16 +0200

    cms rework

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -16,8 +16,8 @@ export default async function Home() {
   return (
     <div className="flex p-5 flex-col gap-4">{
       data.map((item) => <div key={item.id} className="border border-gray-400 p-5 rounded max-w-[400px]">
-	<div>{item.attributes.title}</div>
-	<div>{item.attributes.description}</div>
+	<div>Title: {item.attributes.title}</div>
+	<div>Description: {item.attributes.description}</div>
 	<img src={item.attributes.preview.data.attributes.url} />
       </div>)
     }</div>
